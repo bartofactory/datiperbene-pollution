d38e0956c7d9fb62c1d0f622c5b3f01eb5ec43c0
Bartolomeo Tiralongo <bartolomeo@fiscozen.it>
2021-05-07 17:48:10 +0200

    edits legend

--- resources/windowTemplate.js
+++ resources/windowTemplate.js
@@ -110,15 +110,20 @@ function legendTemplateMortalita(){
                     
                 </div>
 
-                <div id="legend" class="row space-around data">
-                    <div><img src="styles/legend/Milano_mortalita_2_0.png" /> <br/>0 - 20</div>
-                    <div><img src="styles/legend/Milano_mortalita_2_1.png" /> <br/>20 - 40</div>
-                    <div><img src="styles/legend/Milano_mortalita_2_2.png" /> <br/>40 - 60</div>
-                    <div><img src="styles/legend/Milano_mortalita_2_3.png" /> <br/>60 - 80</div>
-                    <div><img src="styles/legend/Milano_mortalita_2_4.png" /> <br/>80 - 100</div>
-                    <div><img src="styles/legend/Milano_mortalita_2_5.png" /> <br/>100 - 120</div>
-                    <div><img src="styles/legend/Milano_mortalita_2_6.png" /> <br/>120 - 139</div>
+                <div id="legend" class="row space-around dataLegend">
+                    <div>
+                        <img src="styles/legend/Milano_mortalita_2_0.png" />  0 - 20 <br/>
+                        <img src="styles/legend/Milano_mortalita_2_1.png" />  20 - 40 <br/>
+                        <img src="styles/legend/Milano_mortalita_2_2.png" />  40 - 60 <br/>
+                        <img src="styles/legend/Milano_mortalita_2_3.png" />  60 - 80 <br/>
+                    </div>
+                    <div>
+                        <img src="styles/legend/Milano_mortalita_2_4.png" />  80 - 100 <br/>
+                        <img src="styles/legend/Milano_mortalita_2_5.png" />  100 - 120 <br/>
+                        <img src="styles/legend/Milano_mortalita_2_6.png" />  120 - 139 <br/>
+                    </div>
                 </div>
+                
             </section>
             <section id="footer">
                 <div class="row">
