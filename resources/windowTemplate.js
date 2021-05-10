855f5a4a50be8bfcda89d3442fdc84cef59315a0
Bartolomeo Tiralongo <bartolomeo@fiscozen.it>
2021-05-10 13:01:37 +0200

    edits concentration map and legend

--- resources/windowTemplate.js
+++ resources/windowTemplate.js
@@ -153,8 +153,8 @@ function legendTemplateNOx(){
                     <div>
                         <img src="./images/gradient_NO2.jpeg" class="gradient"/> 
                         <div class="row space-between">
-                            <span>32,7</span>
-                            <span>59,3</span>
+                            <span>20</span>
+                            <span>60</span>
                         </div>
                     </div>
                 </div>
