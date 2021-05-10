b2568e78daf7686f4e9ea24ef1834744adf77df7
Bartolomeo Tiralongo <bartolomeo@fiscozen.it>
2021-05-10 12:57:57 +0200

    edits confidence values

--- resources/popupFiller.js
+++ resources/popupFiller.js
@@ -19,9 +19,9 @@ function generatePopup(event){
         params.zoneName = evt.values_.NIL_lower || ''
         var mortalita_100 = confidence.find(el => el.NIL_lower === params.zoneName)
         params.population = evt.values_.Pop || ''
-        params.died100.confidenceHigh = Math.round(mortalita_100.tasso_sup * 10)
-        params.died100.confidenceLow = Math.round(mortalita_100.tasso_inf * 10)
-        params.died100.value = Math.round(mortalita_100.tasso * 10)
+        params.died100.confidenceHigh = Math.round(mortalita_100.tasso_sup)
+        params.died100.confidenceLow = Math.round(mortalita_100.tasso_inf)
+        params.died100.value = Math.round(mortalita_100.tasso)
         params.dec_attrno2.confidenceHigh = Math.round(mortalita_100.dec_attrno2_sup)
         params.dec_attrno2.confidenceLow = Math.round(mortalita_100.dec_attrno2_inf)
         params.dec_attrno2.value = Math.round(mortalita_100.dec_attrno2)
