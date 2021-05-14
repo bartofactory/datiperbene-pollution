7849fdb394a36ee8166888ab700d6447747d562e
Bartolomeo Tiralongo <bartolomeo@fiscozen.it>
2021-05-14 14:47:53 +0200

    edits image resolution + CPA style changes

--- layers/layers.js
+++ layers/layers.js
@@ -1,49 +1,48 @@
 var wms_layers = [];
 
 
-        var lyr_OpenStreetMap_0 = new ol.layer.Tile({
-            'title': 'OpenStreetMap',
-            'type': 'base',
-            'opacity': 1.000000,
-            
-            
-            source: new ol.source.XYZ({
-    attributions: ' ',
-                url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-            })
-        });
+var lyr_OpenStreetMap_0 = new ol.layer.Tile({
+    'title': 'OpenStreetMap',
+    'type': 'base',
+    'opacity': 1.000000,
 
 
-var colorize = new ol.filter.Colorize({ operation:'grayscale', value: 1 });
+    source: new ol.source.XYZ({
+        attributions: ' ',
+        url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+    })
+});
+
+
+var colorize = new ol.filter.Colorize({ operation: 'grayscale', value: 1 });
 lyr_OpenStreetMap_0.addFilter(colorize);
 
 var lyr_MI_NO2_1 = new ol.layer.Image({
-                            opacity: 1,
-                            title: "MI_NO2",
-                            opacity: 0.750000,
-                            
-                            
-                            source: new ol.source.ImageStatic({
-                               url: "./layers/MI_NO2_1.png",
-    attributions: ' ',
-                                projection: 'EPSG:3857',
-                                alwaysInRange: true,
-                                imageExtent: [1360005.072297, 5108901.027018, 1432151.705687, 5183929.681828]
-                            })
-                        });
+    opacity: 1,
+    title: "MI_NO2",
+    opacity: 0.750000,
+
+
+    source: new ol.source.ImageStatic({
+        url: "./layers/MI_NO2_1.png?no2",
+        attributions: ' ',
+        projection: 'EPSG:3857',
+        alwaysInRange: true,
+        imageExtent: [1360005.072297, 5108901.027018, 1432151.705687, 5183929.681828]
+    })
+});
 var format_Milano_mortalita_2 = new ol.format.GeoJSON();
-var features_Milano_mortalita_2 = format_Milano_mortalita_2.readFeatures(json_Milano_mortalita_2, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var features_Milano_mortalita_2 = format_Milano_mortalita_2.readFeatures(json_Milano_mortalita_2, { dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857' });
 var jsonSource_Milano_mortalita_2 = new ol.source.Vector({
     attributions: ' ',
 });
 jsonSource_Milano_mortalita_2.addFeatures(features_Milano_mortalita_2);
 var lyr_Milano_mortalita_2 = new ol.layer.Vector({
-                declutter: true,
-                source:jsonSource_Milano_mortalita_2, 
-                style: style_Milano_mortalita_2,
-                'opacity': 0.800000,
-                interactive: true,
+    declutter: true,
+    source: jsonSource_Milano_mortalita_2,
+    style: style_Milano_mortalita_2,
+    'opacity': 0.800000,
+    interactive: true,
     title: 'Milano_mortalita<br />\
     <img src="styles/legend/Milano_mortalita_2_0.png" /> 0 - 20<br />\
     <img src="styles/legend/Milano_mortalita_2_1.png" /> 20 - 40<br />\
@@ -55,13 +54,15 @@ var lyr_Milano_mortalita_2 = new ol.layer.Vector({
     <img src="styles/legend/Milano_mortalita_2_7.png" /> 140 - 160<br />\
     <img src="styles/legend/Milano_mortalita_2_8.png" /> 160 - 180<br />\
     <img src="styles/legend/Milano_mortalita_2_9.png" /> 180 - 199<br />'
-        });
+});
 
-        lyr_OpenStreetMap_0.setVisible(true);lyr_MI_NO2_1.setVisible(true);lyr_Milano_mortalita_2.setVisible(true);
-        var layersList = [lyr_OpenStreetMap_0,lyr_MI_NO2_1,lyr_Milano_mortalita_2];
-        lyr_Milano_mortalita_2.set('fieldAliases', {'ID_NIL': 'ID_NIL', 'AreaHA': 'AreaHA', 'AreaMQ': 'AreaMQ', 'NIL_lower': 'NIL_lower', 'Pop': 'Pop', 'c20_tasso': 'c20_tasso', 'c40_tasso': 'c40_tasso', });
-        lyr_Milano_mortalita_2.set('fieldImages', {'ID_NIL': 'Range', 'AreaHA': 'TextEdit', 'AreaMQ': 'TextEdit', 'NIL_lower': 'TextEdit', 'Pop': 'TextEdit', 'c20_tasso': 'TextEdit', 'c40_tasso': 'TextEdit', });
-        lyr_Milano_mortalita_2.set('fieldLabels', {'ID_NIL': 'inline label', 'AreaHA': 'inline label', 'AreaMQ': 'inline label', 'NIL_lower': 'header label', 'Pop': 'inline label', 'c20_tasso': 'header label', 'c40_tasso': 'inline label', });
-        lyr_Milano_mortalita_2.on('precompose', function(evt) {
-            evt.context.globalCompositeOperation = 'normal';
-        });
+lyr_OpenStreetMap_0.setVisible(true);
+lyr_MI_NO2_1.setVisible(true);
+lyr_Milano_mortalita_2.setVisible(true);
+var layersList = [lyr_OpenStreetMap_0, lyr_MI_NO2_1, lyr_Milano_mortalita_2];
+lyr_Milano_mortalita_2.set('fieldAliases', { 'ID_NIL': 'ID_NIL', 'AreaHA': 'AreaHA', 'AreaMQ': 'AreaMQ', 'NIL_lower': 'NIL_lower', 'Pop': 'Pop', 'c20_tasso': 'c20_tasso', 'c40_tasso': 'c40_tasso', });
+lyr_Milano_mortalita_2.set('fieldImages', { 'ID_NIL': 'Range', 'AreaHA': 'TextEdit', 'AreaMQ': 'TextEdit', 'NIL_lower': 'TextEdit', 'Pop': 'TextEdit', 'c20_tasso': 'TextEdit', 'c40_tasso': 'TextEdit', });
+lyr_Milano_mortalita_2.set('fieldLabels', { 'ID_NIL': 'inline label', 'AreaHA': 'inline label', 'AreaMQ': 'inline label', 'NIL_lower': 'header label', 'Pop': 'inline label', 'c20_tasso': 'header label', 'c40_tasso': 'inline label', });
+lyr_Milano_mortalita_2.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
--- resources/functions.js
+++ resources/functions.js
@@ -1,10 +1,10 @@
 var createTextStyle = function(feature, resolution, labelText, labelFont,
-                               labelFill, placement, bufferColor,
-                               bufferWidth) {
+    labelFill, placement, bufferColor,
+    bufferWidth) {
 
     if (feature.hide || !labelText) {
-        return; 
-    } 
+        return;
+    }
 
     if (bufferWidth == 0) {
         var bufferStyle = null;
@@ -14,7 +14,7 @@ var createTextStyle = function(feature, resolution, labelText, labelFont,
             width: bufferWidth
         })
     }
-    
+
     var textStyle = new ol.style.Text({
         font: labelFont,
         text: labelText,
@@ -25,7 +25,7 @@ var createTextStyle = function(feature, resolution, labelText, labelFont,
         placement: placement,
         maxAngle: 0,
         fill: new ol.style.Fill({
-          color: labelFill
+            color: labelFill
         }),
         stroke: bufferStyle
     });
@@ -48,82 +48,82 @@ function stripe(stripeWidth, gapWidth, angle, color) {
     outerCanvas.width = screen.width;
     outerCanvas.height = screen.height;
     outerContext.rotate((Math.PI / 180) * angle);
-    outerContext.translate(-(screen.width/2), -(screen.height/2));
+    outerContext.translate(-(screen.width / 2), -(screen.height / 2));
     outerContext.fillStyle = innerPattern;
-    outerContext.fillRect(0,0,screen.width,screen.height);
+    outerContext.fillRect(0, 0, screen.width, screen.height);
 
     return outerContext.createPattern(outerCanvas, 'no-repeat');
 };
 
-function closeWindow(htmlContent){
+function closeWindow(htmlContent) {
     document.getElementById('modal').innerHTML = htmlContent || ''
 }
 
-function openWindow(htmlContent){
+function openWindow(htmlContent) {
     document.getElementById('modal').innerHTML = htmlContent
 }
 
-function visibleOnlyMortalita(){
+function visibleOnlyMortalita() {
     lyr_MI_NO2_1.setVisible(false);
     lyr_Milano_mortalita_2.setVisible(true);
-    document.title = 'Mortalità Per NO2 a Roma'
+    document.title = 'Mortalità\\anno per NO2 a Roma'
 }
 
-function visibileOnlyNOx(){
+function visibileOnlyNOx() {
     lyr_MI_NO2_1.setVisible(true);
     lyr_Milano_mortalita_2.setVisible(false);
     document.title = 'Concentrazione di NO2 a Roma'
 }
 
-function goToNOx(){
+function goToNOx() {
     visibileOnlyNOx();
     closeWindow(legendTemplateNOx())
 }
 
-function goToMortalita(){
+function goToMortalita() {
     visibleOnlyMortalita();
     closeWindow(legendTemplateMortalita())
 }
 
-function legendTemplate(){
-    if(lyr_MI_NO2_1.getVisible())
+function legendTemplate() {
+    if (lyr_MI_NO2_1.getVisible())
         return legendTemplateNOx();
     else
         return legendTemplateMortalita();
 }
 
-function toggleBottomSheet(){
+function toggleBottomSheet() {
     var arrowUp = document.getElementsByClassName('gg-chevron-up')[0]
     var arrowDown = document.getElementsByClassName('gg-chevron-down')[0]
 
-    if(document.getElementsByClassName('invisible').length > 0){
+    if (document.getElementsByClassName('invisible').length > 0) {
         document.getElementsByClassName('invisible')[0].classList.add('visible');
         document.getElementsByClassName('invisible')[0].classList.remove('invisible');
         arrowUp.classList.add('gg-chevron-down')
         arrowUp.classList.remove('gg-chevron-up')
-    }else if(document.getElementsByClassName('visible').length > 0){
+    } else if (document.getElementsByClassName('visible').length > 0) {
         document.getElementsByClassName('visible')[0].classList.add('invisible');
         document.getElementsByClassName('visible')[0].classList.remove('visible');
         arrowDown.classList.add('gg-chevron-up')
         arrowDown.classList.remove('gg-chevron-down')
     }
-    
+
 }
 
-function loadLegend(){
+function loadLegend() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
 
-    if(urlParams.get('no-overlay') === 'true'){
+    if (urlParams.get('no-overlay') === 'true') {
         var controls = document.getElementsByClassName('ol-control')
         document.getElementById('modal').style.display = 'none'
-        for(var i = 0; i < controls.length; i++)
+        for (var i = 0; i < controls.length; i++)
             controls[i].style.display = 'none'
     }
 
-    if(urlParams.get('map-type') === 'concentrazione'){
+    if (urlParams.get('map-type') === 'concentrazione') {
         goToNOx();
-    }else{
+    } else {
         goToMortalita();
     }
 
